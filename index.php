11f4fc8654dde5ba8c2e3d50ae8c3138b2f5d145
Jeff Fan <chen-fu.fan@scout24.com>
2020-12-08 23:10:04 +0100

    Added category and display thumbnail

--- index.php
+++ index.php
@@ -8,4 +8,7 @@ $userLoggedIn = $_SESSION["userLoggedIn"];
 $preview = new PreviewProvider($con, $userLoggedIn);
 echo $preview->createPreviewVideo(null);
 
+$containers = new CategoryContainers($con, $userLoggedIn);
+echo $containers->showAllCategories();
+
 ?>
