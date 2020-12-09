1fe0a3fc7b42eded0e7130eaf1812d0652ae2d66
Jeff Fan <chen-fu.fan@scout24.com>
2020-12-09 22:09:39 +0100

    Added seasons, videos

--- includes/header.php
+++ includes/header.php
@@ -5,10 +5,16 @@ require_once("includes/classes/PreviewProvider.php");
 require_once("includes/classes/CategoryContainers.php");
 require_once("includes/classes/Entity.php");
 require_once("includes/classes/EntityProvider.php");
+require_once("includes/classes/ErrorMessage.php");
+require_once("includes/classes/SeasonProvider.php");
+require_once("includes/classes/Season.php");
+require_once("includes/classes/Video.php");
 
 if(!isset($_SESSION["userLoggedIn"])) {
     header("Location: register.php");
 }
+$userLoggedIn = $_SESSION["userLoggedIn"];
+
 ?>
 
 <!DOCTYPE html>
--- index.php
+++ index.php
@@ -3,8 +3,6 @@ require_once("includes/header.php");
 
 
 
-$userLoggedIn = $_SESSION["userLoggedIn"];
-
 $preview = new PreviewProvider($con, $userLoggedIn);
 echo $preview->createPreviewVideo(null);
 
